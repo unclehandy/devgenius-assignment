54bcf25ff3abbfede3c8506359bd071520d0e372
Handy Pratama <handypratamacikal.co.id@192.168.1.5>
2024-02-14 22:16:26 +0700

    feat: mapping data to search result UI

--- src/components/ListEvents.jsx
+++ src/components/ListEvents.jsx
@@ -1,18 +1,32 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { eventDataAtom } from "./Atom/EventDataAtom";
 
 export const ListEvents = ({ dataEvents }) => {
-  const dataEventsFilter = dataEvents.filter(function (itemFilter) {
-    return  itemFilter.events.author == "ds_v3jTVjbKWukzTUd" || itemFilter.events.author == "ds_MqBbtrypLFQ6X3P" || itemFilter.events.author == "ds_FPFzoy8P0wqCDBl"
-  })
+  const [, setDataEvents] = useAtom(eventDataAtom);
 
+  useEffect(() => {
+    setDataEvents(dataEvents);
+  }, [dataEvents, setDataEvents]);
+
+  const dataEventsFilter = dataEvents.filter(function (itemFilter) {
+    return (
+      itemFilter.events.author == "ds_v3jTVjbKWukzTUd" ||
+      itemFilter.events.author == "ds_MqBbtrypLFQ6X3P" ||
+      itemFilter.events.author == "ds_FPFzoy8P0wqCDBl"
+    );
+  });
 
   const shortenTitle = (title, maxLength) => {
     if (title.length <= maxLength) return title;
-    const lastSpaceIndex = title.lastIndexOf(' ', maxLength);
-    return title.slice(0, lastSpaceIndex > 0 ? lastSpaceIndex : maxLength) + '...';
+    const lastSpaceIndex = title.lastIndexOf(" ", maxLength);
+    return (
+      title.slice(0, lastSpaceIndex > 0 ? lastSpaceIndex : maxLength) + "..."
+    );
   };
 
   return (
@@ -20,26 +34,31 @@ export const ListEvents = ({ dataEvents }) => {
       <main className="card lg:grid lg:grid-cols-5 gap-6 p-5">
         {dataEventsFilter.map((item) => {
           return (
-
             <div className="card  bg-slate-800 shadow border border-slate-900 hover:scale-105 transition duration-500 overflow-hidden ">
-              <div className='w-full h-40 overflow-hidden '>
+              <div className="w-full h-40 overflow-hidden ">
                 <img
                   src={item.events.image}
-                  className='w-auto h-full  object-cover'
+                  className="w-auto h-full  object-cover"
                 />
               </div>
               <div className="space-y-2 p-3">
-                <h2 className="card-title text-amber-400">{shortenTitle(item.events.title, 20)}</h2>
+                <h2 className="card-title text-amber-400">
+                  {shortenTitle(item.events.title, 20)}
+                </h2>
                 <p> {item.events.dateTime}</p>
                 <div className="card-actions justify-end">
-                  <Link className="btn-utama " href={`/events/${item.events.id}`}>Detail Event</Link>
+                  <Link
+                    className="btn-utama "
+                    href={`/events/${item.events.id}`}
+                  >
+                    Detail Event
+                  </Link>
                 </div>
-
               </div>
             </div>
-          )
+          );
         })}
       </main>
     </>
-  )
-}
+  );
+};
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,18 +1,71 @@
 "use client";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { useState } from "react";
+import { useAtom } from "jotai";
+import { eventDataAtom } from "./Atom/EventDataAtom";
 
-export const SearchResult = ({ dataLookup }) => {
+export const SearchResult = () => {
+  const [hoverStates, setHoverStates] = useState(Array(2).fill(false));
+  const [allDataEvents] = useAtom(eventDataAtom);
+
+  const dataEventsFilter = allDataEvents.filter(function (itemFilter) {
+    return (
+      itemFilter.events.author == "ds_v3jTVjbKWukzTUd" ||
+      itemFilter.events.author == "ds_MqBbtrypLFQ6X3P" ||
+      itemFilter.events.author == "ds_FPFzoy8P0wqCDBl"
+    );
+  });
+
+  const handleMouseEnter = (index) => {
+    const newHoverStates = [...hoverStates];
+    newHoverStates[index] = true;
+    setHoverStates(newHoverStates);
+    console.log(index);
+    console.log(newHoverStates);
+  };
+
+  const handleMouseLeave = (index) => {
+    const newHoverStates = [...hoverStates];
+    newHoverStates[index] = false;
+    setHoverStates(newHoverStates);
+  };
+
+  const arrowColor = {
+    default: "#A1A1AA",
+    hover: "#d97706",
+  };
+
   return (
     <div className="rounded-xl w-[400px] absolute top-14 z-10 bg-slate-50 text-sm text-blue mt-2 flex-col space-y-2 max-h-60 overflow-y-scroll">
-      <div className="py-4 px-6  hover:bg-blue-100 flex justify-between items-center">
-        <div className="w-20 h-16 bg-blue-300 rounded-lg"></div>
-        <div className="flex-col space-y-2 -ml-10">
-          <p className="text-black text-sm">Kampanye Tandingan</p>
-          <p className="text-black text-xs">9 Februari 2024</p>
-        </div>
-        <ArrowRight className="text-black" />
-      </div>
+      {dataEventsFilter.map((event, index) => {
+        return (
+          <Link href={`/events/${event.events.id}`}>
+            <div
+              key={index}
+              className="py-4 px-6 flex justify-between items-center hover:bg-gray-100"
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave(index)}
+            >
+              <img
+                src={event.events.image}
+                className="w-20 h-16 bg-blue-300 rounded-lg object-cover"
+              ></img>
+              <div className="flex-col space-y-2 basis-1/2">
+                <p className="text-black text-sm line-clamp-1">
+                  {event.events.title}
+                </p>
+                <p className="text-black text-xs">{event.events.dateTime}</p>
+              </div>
+              <ArrowRight
+                color={
+                  hoverStates[index] ? arrowColor.hover : arrowColor.default
+                }
+              />
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
