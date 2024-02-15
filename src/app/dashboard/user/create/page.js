d6cb830f465acafd19ef7a205e515b2b696b0a21
Handy Pratama <handypratamacikal.co.id@192.168.1.8>
2024-02-15 19:27:04 +0700

    feat: adding npm run build

--- src/app/dashboard/user/create/page.js
+++ src/app/dashboard/user/create/page.js
@@ -1,60 +1,53 @@
-"use client"
+"use client";
 import FormUser from "@/components/formUser/page";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import Swal from 'sweetalert';
-
+import Swal from "sweetalert2";
 
 export default function CreateUser() {
-    const router = useRouter();
+  const router = useRouter();
 
-    async function handleSubmitRegister(event){
+  async function handleSubmitRegister(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-        event.preventDefault();
-        const name = event.target.name.value;
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+    const options = {
+      method: "POST",
+      url: "https://eventmakers-api.fly.dev/auth/register",
+      headers: { "Content-Type": "application/json" },
+      data: { name: name, email: email, password: password },
+    };
 
-        const options = {
-            method: 'POST',
-            url: 'https://eventmakers-api.fly.dev/auth/register',
-            headers: {'Content-Type': 'application/json'},
-            data: {name: name, email: email, password: password}
-          };
-          
-          try {
-            const { data } = await axios.request(options);
-            console.log(data);
+    try {
+      const { data } = await axios.request(options);
+      console.log(data);
 
-            Swal({
-                title: "Hello!",
-                text: "Kamu sudah berhasil mendaftar",
-                icon: "success",
-              }).then(() => {
-               
-                router.push("/dashboard/user");
-                router.refresh();
-              });
+      Swal({
+        title: "Hello!",
+        text: "Kamu sudah berhasil mendaftar",
+        icon: "success",
+      }).then(() => {
+        router.push("/dashboard/user");
+        router.refresh();
+      });
 
-            event.target.name.value = "";
-            event.target.email.value = "";
-            event.target.password.value = "";
-          } catch (error) {
-            console.error(error);
-          }
+      event.target.name.value = "";
+      event.target.email.value = "";
+      event.target.password.value = "";
+    } catch (error) {
+      console.error(error);
     }
+  }
 
+  return (
+    <div className="card w-full bg-neutral m-0">
+      <div className="card-body">
+        <h2 className="card-title">Add User</h2>
 
-    return (
-        
-            <div className="card w-full bg-neutral m-0">
-                <div className="card-body">
-                    <h2 className="card-title">Add User</h2>
-                 
-                    <FormUser onSubmit={handleSubmitRegister}/>
-                  
-                </div>
-            </div>
-        
-    )
+        <FormUser onSubmit={handleSubmitRegister} />
+      </div>
+    </div>
+  );
 }
