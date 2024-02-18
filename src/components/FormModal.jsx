c74d1e7072be9b3700f81ea833d537be957cc394
Handy Pratama <handypratamacikal.co.id@192.168.1.8>
2024-02-18 13:51:23 +0700

    feat: adding join event functionality

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -1,6 +1,16 @@
 "use client";
+import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
+import { X } from "lucide-react";
 
 export const FormModal = ({ idEvent }) => {
+  const router = useRouter();
+
+  const handleCloseModal = () => {
+    const modal = document.getElementById("form-pop-up");
+    modal.close();
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
@@ -10,11 +20,13 @@ export const FormModal = ({ idEvent }) => {
     async function registerEvent(idEvent, name, email, phone) {
       try {
         const res = await fetch(
-          `https://eventmakers-api.fly.dev/documentation#tag/events/post/events/${idEvent}/join`,
+          `https://eventmakers-api.fly.dev/events/${idEvent}/join`,
           {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRzX01xQmJ0cnlwTEZRNlgzUCIsIm5hbWUiOiJyb25hbCB5dWx5YW50byIsImVtYWlsIjoic3VyZWwucm9uYWxAZ21haWwuY29tIiwiYXZhdGFyIjpudWxsLCJpYXQiOjE3MDc2NDAzNjAsImV4cCI6MTcxODAwODM2MH0.eEyXTA8CFQK1durOGksUsGxeuxaay0tTqvqyA3yP8IY",
             },
             body: JSON.stringify({
               name: name,
@@ -30,17 +42,31 @@ export const FormModal = ({ idEvent }) => {
       }
     }
     let data = registerEvent(idEvent, name, email, phone);
-    console.log(data);
+    handleCloseModal();
+
+    Swal.fire({
+      title: "Success",
+      text: "Berhasil join event",
+      icon: "success",
+    });
+    router.refresh();
   };
 
   return (
-    <dialog id="form-pop-up" className="modal bg-slate-800 bg-opacity-70">
-      <div className="w-full p-4">
-        <div className="text-center font-semibold text-xl">
-          Lengkapi data dirimu dulu ya..
-        </div>
+    <dialog id="form-pop-up" className="modal">
+      <div className="p-4 w-[60%]">
         <div className="modal-action justify-center">
-          <form className="w-[60%]" onSubmit={handleSubmit}>
+          <form
+            className="w-full bg-slate-800 p-20 relative"
+            onSubmit={handleSubmit}
+          >
+            <X
+              className="absolute right-10 top-10 cursor-pointer hover:text-red-300"
+              onClick={handleCloseModal}
+            />
+            <div className="text-center font-semibold text-xl">
+              Lengkapi data dirimu dulu ya..
+            </div>
             <div>
               <label className="label">
                 <span className="text-base label-text">Name</span>
