d503216cc9bb8b9c3717aa980b1706b988722a5d
fikryhaykal <hykl3107@gmail.com>
2024-02-18 12:58:40 +0800

    feature:dashboard and fixed some ui

--- src/app/dashboard/home/page.js
+++ src/app/dashboard/home/page.js
@@ -1,9 +1,70 @@
+"use client"
+import { useEffect } from "react";
+import { Chart } from "chart.js";
+
+async function fetchData() {
+    const res = await fetch('https://eventmakers-api.fly.dev/events/', {
+        cache: "no-cache",
+    });
+
+    const data = await res.json();
+    return data;
+}
 
 export default function Dashboard() {
-    return (
-        <div className="flex flex-rows justify-between ">
-            aaa sss
-        </div>
+    useEffect(() => {
+        const fetchEventData = async () => {
+            const data = await fetchData();
+            const events = data.data;
 
-    )
+            // Filter events based on author
+            const filteredEvents = events.filter(event => {
+                return (
+                    event.events.author === "ds_v3jTVjbKWukzTUd" ||
+                    event.events.author === "ds_MqBbtrypLFQ6X3P" ||
+                    event.events.author === "ds_FPFzoy8P0wqCDBl"
+                );
+            });
+
+            const eventNames = filteredEvents.map(event => event.events.title);
+            const participantsCounts = filteredEvents.map(event => event.participants.length);
+
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: eventNames,
+                    datasets: [{
+                        label: "Number of Participants",
+                        data: participantsCounts,
+                        borderColor: "#ffa500",
+                        backgroundColor: "#3e95cd",
+                        fill: false,
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            title: {
+                                display: true,
+                                text: 'Number of Participants',
+                            },
+                        },
+                    },
+                },
+            });
+        };
+
+        fetchEventData();
+    }, []);
+
+    return (
+        <div className="w-full h-screen flex mx-auto my-auto">
+            <div className='border border-gray-400 pt-0 rounded-xl w-full h-fit my-auto shadow-xl'>
+                <canvas id='myChart'></canvas>
+            </div>
+        </div>
+    );
 }
+
